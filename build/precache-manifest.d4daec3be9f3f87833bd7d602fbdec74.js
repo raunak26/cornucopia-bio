4f627d71423eeb475c29cbfa9153cdd3917efe4c
Raunak Anand <46580556+raunak26@users.noreply.github.com>
2025-02-10 19:22:56 -0800

    fixed build

---
+++ build/precache-manifest.d4daec3be9f3f87833bd7d602fbdec74.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3caa52034dccec45937fc4098be50130",
+    "url": "/index.html"
+  },
+  {
+    "revision": "8d3233a56921c12cf87a",
+    "url": "/static/css/2.83d756e6.chunk.css"
+  },
+  {
+    "revision": "408cf0ffb1f30d8ee268",
+    "url": "/static/css/main.8572429d.chunk.css"
+  },
+  {
+    "revision": "8d3233a56921c12cf87a",
+    "url": "/static/js/2.d7aaad3f.chunk.js"
+  },
+  {
+    "revision": "8fbaafc1bcd125115286065c8a56ff3a",
+    "url": "/static/js/2.d7aaad3f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6e9a185798703af6e288",
+    "url": "/static/js/3.611fc9c0.chunk.js"
+  },
+  {
+    "revision": "408cf0ffb1f30d8ee268",
+    "url": "/static/js/main.078d3403.chunk.js"
+  },
+  {
+    "revision": "7551164497ff8606fd05",
+    "url": "/static/js/runtime-main.ff142690.js"
+  },
+  {
+    "revision": "05e17c046727a3defe8899bf3883d2fd",
+    "url": "/static/media/report.05e17c04.svg"
+  }
+]);
